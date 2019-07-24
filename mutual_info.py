fa2bcfdca7dfbe13231d43b0405d5fa2a9076783
Codie Wood <codiejcwood@yahoo.co.uk>
2019-07-24 16:24:10 +0100

    updated to object oriented code and ran new plots with thresholds

--- mutual_info.py
+++ mutual_info.py
@@ -1,98 +1,47 @@
 import gzip, json, re, sys, math, os
 from collections import Counter
+import oas_file_handling as oas
 
-#oas_file = str(sys.argv[1])
-#oas_file = "Vander_Heiden_2017_Heavy_HD09_IGHG_HD09_Unsorted_Bcells_age31_healthy_iglblastn_igblastn_IGHG.json.gz"
-oas_file = "Corcoran_2016_heavy_mouse_IGHG_mouse_heavy_M2_igblastn_igblastn_IGHG.json.gz"
-
-meta_line = True
-sequence_data = []
-for line in gzip.open(oas_file, 'rb'):
-    if meta_line == True:
-        metadata = json.loads(line)
-        meta_line = False
-        continue
-    
-    #Parse actual sequence data.
-    basic_data = json.loads(line)
-    sequence_data.append(basic_data)
-    
-    #IMGT-Numbered sequence.
-    d = json.loads(basic_data['data'])
-    sequence_data[-1]['data'] = d
-    
-def maximum_valued_key(dictionary):
-    values = list(dictionary.values())
-    keys = list(dictionary.keys())
-    return keys[values.index(max(values))]
-
-def sort_alphanumerically(l): 
-    convert = lambda text: int(text) if text.isdigit() else text.lower() 
-    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
-    return sorted(l, key = alphanum_key)
-
-redundant_sequences = metadata['Size']
-regions = ['fwh1','cdrh1','fwh2','cdrh2','fwh3','cdrh3','fwh4']
-amino_acid_list = ['A','G','I','L','P','V','F','W','Y','D','E','S','T','R','H','K','C','M','N','Q','Unused']
-
-def combine_regions(data):
-    combined_sequence = {}
-    for region_name in regions:
-        region_sequence = data['data'][region_name]
-        combined_sequence.update(region_sequence)
-    return combined_sequence 
-
-def find_amino_acids(position):
-    amino_acids = []
-    for data in sequence_data:
-        full_sequence = combine_regions(data)
-        for i in range(int(data['redundancy'])):
-            if position in full_sequence:
-                amino_acids.append(full_sequence[position])
-            else:
-                amino_acids.append('Unused')
-    return amino_acids
-
-def generate_positions():
-    all_positions = set()
-    for data in sequence_data:
-        full_sequence = combine_regions(data)
-        positions_used = list(full_sequence.keys())
-        for position in positions_used:
-            all_positions.add(position)
-    position_list = sort_alphanumerically(list(all_positions))
-    return position_list 
-
-position_list = generate_positions()
-
-def amino_acid_percent(position):
-    amino_acids = find_amino_acids(position)
-    amino_acid_count = Counter(amino_acids)
-    for amino_acid in amino_acid_count:
-        frequency = (100*amino_acid_count[amino_acid])/redundant_sequences
-        amino_acid_count[amino_acid] = round(frequency,2)
-    amino_acid_frequency = dict(amino_acid_count)
-    return amino_acid_frequency
+#f = oas.oas_file("Vander_Heiden_2017_Heavy_HD09_IGHG_HD09_Unsorted_Bcells_age31_healthy_iglblastn_igblastn_IGHG.json.gz")
+#f = oas.oas_file("Corcoran_2016_heavy_mouse_IGHG_mouse_heavy_M2_igblastn_igblastn_IGHG.json.gz")
+f = oas.oas_file(str(sys.argv[1]))
+if len(sys.argv) > 2:
+    threshold = int(sys.argv[2])
+else:
+    threshold = 1
 
 def probability(position,amino_acid):
-    amino_acid_frequency = amino_acid_percent(position)
+    amino_acid_frequency = f.amino_acid_frequency(position)
     if amino_acid in amino_acid_frequency:
-        prob = amino_acid_percent(position)[amino_acid]/100
+        prob = amino_acid_frequency[amino_acid]/100
     else:
         prob = 0
     return prob
 
-def conditional_probability(X,x,Y,y): #prob that X=x given Y=y; X,Y positions and x,y amino acids
+def conditional_probability(position,amino_acid,given_position,given_amino_acid):
     condition = 0
     both = 0
-    for data in sequence_data:
-        full_sequence = combine_regions(data)
-        if Y in full_sequence and full_sequence[Y] == y:
+    for data in f.sequence_data:
+        full_sequence = f.combined_sequence(data)
+        if given_amino_acid == 'Unused':
+            if given_position not in full_sequence:
+                condition += 1
+                if amino_acid == 'Unused':
+                    if position not in full_sequence:
+                        both += 1
+                elif position in full_sequence and full_sequence[position] == amino_acid:
+                    both += 1
+        elif given_position in full_sequence and full_sequence[given_position] == given_amino_acid:
             condition += 1
-            if X in full_sequence and full_sequence[X] == x:
+            if amino_acid == 'Unused':
+                if position not in full_sequence:
+                        both += 1
+            elif position in full_sequence and full_sequence[position] == amino_acid:
                 both += 1
+            
     if condition == 0:
-        cond_prob = 0
+        cond_prob = "Conditioned on an impossible event, {} = {}".format(given_position,given_amino_acid)
+        print(cond_prob)
     else:
         cond_prob = both/condition
     return cond_prob
@@ -101,27 +50,30 @@ def joint_probability(X,x,Y,y):
     joint_prob = probability(Y,y)*conditional_probability(X,x,Y,y)
     return joint_prob
 
-def mutual_information(X,Y):
+def mutual_information(position1,position2):
     MI = 0
-    for amino_acid1 in amino_acid_list:
-        for amino_acid2 in amino_acid_list:
-            jp = joint_probability(X,amino_acid1,Y,amino_acid2)
-            px = probability(X,amino_acid1)
-            py = probability(Y,amino_acid2)
-            if px*py == 0 or jp == 0:
+    aa_position1 = set(f.find_amino_acids(position1))
+    aa_position2 = set(f.find_amino_acids(position2))
+    for amino_acid1 in aa_position1:
+        for amino_acid2 in aa_position2:
+            jp = joint_probability(position1,amino_acid1,position2,amino_acid2)
+            p1 = probability(position1,amino_acid1)
+            p2 = probability(position2,amino_acid2)
+            if jp == 0:
                 MI += 0
             else:
-                MI += jp*math.log(jp/(px*py))
+                MI += jp*math.log(jp/(p1*p2))
     return MI        
 
 print("""Using data from {}:
-              """.format(oas_file))
+              """.format(f.file_name))
+position_list = f.all_positions(threshold)
 for position1 in position_list:
     for position2 in position_list:
         MI = mutual_information(position1,position2)
         if position1 == position2:
             print("""The self information of position {} has value {}.
-              """.format(position1,MI))
+                  """.format(position1,MI))
         else:
             print("""The mutual information of positions {} and {} has value {}.
-              """.format(position1,position2,MI))
+                  """.format(position1,position2,MI))
---
+++ oas_file_handling.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import gzip, json, re
+from collections import Counter
+
+def maximum_valued_key(dictionary):
+    values = list(dictionary.values())
+    keys = list(dictionary.keys())
+    return keys[values.index(max(values))]
+
+def sort_alphanumerically(l): 
+    convert = lambda text: int(text) if text.isdigit() else text.lower() 
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key)] 
+    return sorted(l, key = alphanum_key)
+
+class oas_file():
+    def __init__(self, src): 
+        meta_line = True
+        seq_data = []
+        for line in gzip.open(src,'rb'):
+            if meta_line == True:
+                    self.metadata = json.loads(line) 
+                    meta_line=False
+                    continue
+            #Parse actual sequence data.
+            basic_data = json.loads(line)
+            seq_data.append(basic_data)
+            
+            #IMGT-Numbered sequence.
+            d = json.loads(basic_data['data'])
+            seq_data[-1]['data'] = d
+        self.unique_sequences = len(seq_data)
+        self.sequence_data = seq_data
+        if self.metadata['Chain'] == 'Heavy':
+            self.regions = ['fwh1','cdrh1','fwh2','cdrh2','fwh3','cdrh3','fwh4']
+        elif self.metadata['Chain'] == 'Light':
+            self.regions = ['fwl1','cdrl1','fwl2','cdrl2','fwl3','cdrl3','fwl4']
+        self.amino_acids = ['A','G','I','L','P','V','F','W','Y','D','E','S','T','R','H','K','C','M','N','Q','Unused']
+        self.file_name = src
+    
+    def error_counts(self):
+        max_errors = 0
+        correct_sequences = 0
+        for data in self.sequence_data:
+            if data['num_errors'] == "0":
+                correct_sequences += 1
+            elif int(data['num_errors']) > max_errors:
+                max_errors = int(data['num_errors'])
+        erroneous_sequences = self.unique_sequences - correct_sequences
+        
+        print("There are {} sequences thought to contain errors in this data."
+              .format(erroneous_sequences))
+        print("{} sequences are thought to contain 0 errors."
+              .format(correct_sequences))
+        
+        for i in range(1, max_errors + 1):
+            i_errors = 0
+            for data in self.sequence_data:
+                if int(data['num_errors']) == i:
+                    i_errors += 1
+            print("{} sequences are thought to contain {} errors."
+                  .format(i_errors,i))            
+    
+    def region_sequences(self,region_name):
+        region_sequences = []
+        for data in self.sequence_data:
+            region = data['data'][region_name]
+            region_sequences.append(region)
+        return region_sequences
+    
+    def combined_sequence(self, data):
+        combined_sequence = {}
+        for region_name in self.regions:
+            region_sequence = data['data'][region_name]
+            combined_sequence.update(region_sequence)
+        return combined_sequence
+    
+    def most_common_sequence(self, region_name):
+        reg_sequence_counter = {}
+        for seq in self.region_sequences(region_name):
+            key = json.dumps(seq, sort_keys=True)
+            if key in reg_sequence_counter:
+                reg_sequence_counter[key] += 1
+            else:
+                reg_sequence_counter[key] = 1
+        most_common = maximum_valued_key(reg_sequence_counter)
+        occurences = reg_sequence_counter[most_common]
+        print("The {} sequence which was most common was {}, which appeared {} times."
+              .format(region_name,most_common,occurences))
+        
+    def region_positions(self, region_name, threshold=1):
+        region_positions = set()
+        for data in self.region_sequences(region_name):
+                positions_used = list(data.keys())
+                for position in positions_used:
+                    region_positions.add(position)
+        region_position_list = sort_alphanumerically(list(region_positions))
+        if threshold > 1:
+            freq_positions = []
+            for position in region_position_list:
+                position_use_count = 0
+                for data in self.region_sequences(region_name):
+                    if position in data:
+                        position_use_count += 1
+                if position_use_count >= threshold:
+                    freq_positions.append(position)
+            return freq_positions
+        else:
+            return region_position_list
+    
+    def all_positions(self, threshold=1):
+        all_positions = set()
+        for region_name in self.regions:
+            for position in self.region_positions(region_name, threshold):
+                all_positions.add(position)
+        all_positions_list = sort_alphanumerically(list(all_positions))
+        return all_positions_list
+    
+    def find_amino_acids(self, position):
+        amino_acids = []
+        for data in self.sequence_data:
+            if position in self.combined_sequence(data):
+                amino_acids.append(self.combined_sequence(data)[position])
+            else:
+                amino_acids.append('Unused')
+        return amino_acids
+        
+    def amino_acid_frequency(self, position):
+        amino_acids = self.find_amino_acids(position)
+        amino_acid_count = Counter(amino_acids)
+        for amino_acid in amino_acid_count:
+            frequency = (100*amino_acid_count[amino_acid])/self.unique_sequences
+            amino_acid_count[amino_acid] = round(frequency,2)
+        amino_acid_frequency = dict(amino_acid_count)
+        return amino_acid_frequency
+    
+    def print_frequencies(self, threshold=1):
+        freq_dict = {}
+        for position in self.all_positions(threshold):
+            freq_dict[position] = self.amino_acid_frequency(position)
+        return freq_dict
+
+    def consensus_sequence(self, threshold=1):
+        consensus_sequence = {}
+        for position in self.all_positions(threshold):
+            consensus_sequence[position] = maximum_valued_key(self.amino_acid_frequency(position))
+        print("""The consensus sequence for this data, including sequence repeats, is:
+            {}""".format(consensus_sequence))
+            
+            
